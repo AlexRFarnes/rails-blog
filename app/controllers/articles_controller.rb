f4c7a2e60b6af52991a9f16649c0d4d07ce7d4fc
alex <alexrfarnes@gmail.com>
2022-07-08 14:04:42 +0800

    Add the delete method to article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,23 @@
 class ArticlesController < ApplicationController
-    # Una varianle de instancia, @variable, esta disponible dentro de la vista correspondiente, mientras que una variable local, variable, solo esta disponible dentro del controlador
+    # Una variable de instancia, @variable, esta disponible dentro de la vista correspondiente, mientras que una variable local, variable, solo esta disponible dentro del controlador
+    before_action :find_article
      
     def show
         @article = Article.find(params[:id])
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        @article.update(title: params[:article][:title], content: params[:article][:content])
+        redirect_to @article
+    end
+
     def new
         @article = Article.new
-        puts "Hello"
     end
 
     def create
@@ -15,5 +25,15 @@ class ArticlesController < ApplicationController
         @article = Article.create(title: params[:article][:title], content: params[:article][:content])
         # Display in json format
         render json: @article
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to root_path
+    end
+
+    def find_article
+        puts "Estoy en find article"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-      # resource      # who answers to the resource (controller#action)
+  # resource      # who answers to the resource (controller#action)
   get "welcome", to: "home#index"
+  
+  root to: "home#index"
+
   get "articles/new", to: "articles#new"
   get "articles/:id", to: "articles#show"
+  get "articles/:id/edit", to: "articles#edit"
+  patch "articles/:id", to: "articles#update", as: :article
   post "articles", to: "articles#create"
+  delete "articles/:id", to: "articles#destroy"
 end
