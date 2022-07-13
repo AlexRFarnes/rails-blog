ead5d8e233a8cd39ebb44e4a887ffb0efd7e49cb
alex <alexrfarnes@gmail.com>
2022-07-13 19:58:39 +0800

    Configure relation between users and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,8 +7,18 @@
 #  title      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :bigint           default(1), not null
+#
+# Indexes
+#
+#  index_articles_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
 #
 
 class Article < ApplicationRecord
     has_rich_text :content
+    belongs_to :user # additional field
 end
